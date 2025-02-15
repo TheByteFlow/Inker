a9ccaf2adcaabe08ad2c4bfa2a7c8faf521ddbd9
Ijas Ahammed <ahammedijas.official@gmail.com>
2025-02-15 18:29:03 +0530

    Tailwind and Shadcn setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  useEffect(()=>{
-    async function DataFetcher(){
-      const rawResponse=await fetch(`http://localhost:3000`)
-      const jsonResponse=await rawResponse.json()
-      console.log(jsonResponse)
+  useEffect(() => {
+    async function DataFetcher() {
+      const rawResponse = await fetch(`http://localhost:3000`);
+      const jsonResponse = await rawResponse.json();
+      console.log(jsonResponse);
       /*
       @muhammedsirajudeen
       Although its inside the container just call directly to localhost 
       */
     }
-    DataFetcher()
-  },[])
+    DataFetcher();
+  }, []);
   return (
     <>
-    INKER
+    <div className="pl-3">
+
+      <Button>Click me</Button>
+    </div>
+      INKER
     </>
-  )
+  );
 }
 
-export default App
+export default App;
